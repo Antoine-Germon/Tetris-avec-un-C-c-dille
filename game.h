ba0d143b665a0ea4871a2e2e18c31b14f82947eb
jbelhoste <jules.belhoste@etu.unistra.fr>
2025-06-22 14:38:41 +0200

    correction de la gestion de la collisiopn et des out of bound lors de la rotation

--- game.h
+++ game.h
@@ -15,18 +15,12 @@ Tetromino *rotateTetrominoLeft(Board * board);
 
 int isPositionValid(Board * board, Tetromino *tetromino);
 
-int checkLegalRotation(Board * board, Tetromino *tetromino);
-
-void moveTetrominoOutOfBlock(Board * board, Tetromino *tetromino);
-void moveTetrominoInbound(Tetromino *tetromino);
-void moveInboundLeft(Tetromino *tetromino);
-void moveInboundRight(Tetromino *tetromino);
-void moveInboundUp(Tetromino *tetromino);
+int tryMoveTetromino(Board *board, Tetromino *tetromino, int dx, int dy);
 
 void placeTetromino(Board * board);
 
 void moveTetromino(Board * board, int dx, int dy);
-
+int isValidPosition(Board *board, Tetromino *tetromino);
 // Fonctions liées au plateaux
 Board * createBoard(int x, int y);
 void freeBoard(Board * board);
