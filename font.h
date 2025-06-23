1ac5b6d9ebf946ff60035a3bee2c47a4c3b6c134
Antoine Germon <antoine.germon@etu.unistra.fr>
2025-06-23 18:44:06 +0200

    fix ui pas faite pour du dynamique: tailles et positions en dur -> en fonction soit d'un board ou d'un offset x et y

--- font.h
+++ font.h
@@ -7,5 +7,6 @@ SDL_Texture* loadFontTexture(const char* path);
 SDL_Rect getCharSrcRect(unsigned char c);
 void drawChar(unsigned char c, int x, int y, SDL_Color color);
 void drawText(const char* text, int x, int y, SDL_Color color);
+void drawTextSpaced(const char* text, int x, int y, SDL_Color color, int charWidth, int charHeight, int spacing);
 
 #endif // FONT_H
